c45df96338ac0a3690b0e6bc2f4c10b07a24cb5e
snikers231 <karnyuk.alex@gmail.com>
2018-07-11 00:48:20 +0300

    New Commit

--- js/main.js
+++ js/main.js
@@ -1,3 +1,15 @@
 $(function () {
     $("#portfolio__tabs").tabs();
 });
+
+$(document).ready(function () {
+    $('a.scroll-to-next ').click(function () {
+        $('html, body').animate({
+            scrollTop: $($(this).attr('href')).offset().top + 'px'
+        }, {
+            duration: 1000,
+            easing: 'swing'
+        });
+        return false;
+    });
+});
